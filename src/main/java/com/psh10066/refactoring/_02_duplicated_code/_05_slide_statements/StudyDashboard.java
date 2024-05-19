c0ef0d9082da9babd64e3132dccdf35d4de41bd5
이호준 <psh10066@gmail.com>
2024-05-19 16:07:18 +0900

    리팩토링 5. 코드 정리하기

---
+++ src/main/java/com/psh10066/refactoring/_02_duplicated_code/_05_slide_statements/StudyDashboard.java
@@ -0,0 +1,48 @@
+package com.psh10066.refactoring._02_duplicated_code._05_slide_statements;
+
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class StudyDashboard {
+
+    private void printParticipants(int eventId) throws IOException {
+        // Get github issue to check homework
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        GHIssue issue = repository.getIssue(eventId);
+
+        // Get participants
+        // 관련있는 코드끼리 묶여있어야 코드를 더 쉽게 이해할 수 있다.
+        Set<String> participants = new HashSet<>();
+        issue.getComments().forEach(c -> participants.add(c.getUserName()));
+
+        // Print participants
+        participants.forEach(System.out::println);
+    }
+
+    private void printReviewers() throws IOException {
+        // Get github issue to check homework
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        GHIssue issue = repository.getIssue(30);
+
+        // Get reviewers
+        // 관련있는 코드끼리 묶여있어야 코드를 더 쉽게 이해할 수 있다.
+        Set<String> reviewers = new HashSet<>();
+        issue.getComments().forEach(c -> reviewers.add(c.getUserName()));
+
+        // Print reviewers
+        reviewers.forEach(System.out::println);
+    }
+
+    public static void main(String[] args) throws IOException {
+        StudyDashboard studyDashboard = new StudyDashboard();
+        studyDashboard.printReviewers();
+        studyDashboard.printParticipants(15);
+    }
+}
