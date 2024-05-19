84282aae3cad438bd073a831b640a3ba1c6cecb2
이호준 <psh10066@gmail.com>
2024-05-20 00:09:02 +0900

    리팩토링 8. 매개변수 객체 만들기

---
+++ src/main/java/com/psh10066/refactoring/_03_long_function/_08_introdce_parameter_object/Participant.java
@@ -0,0 +1,15 @@
+package com.psh10066.refactoring._03_long_function._08_introdce_parameter_object;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public record Participant(String username, Map<Integer, Boolean> homework) {
+    public Participant(String username) {
+        this(username, new HashMap<>());
+    }
+
+    public void setHomeworkDone(int index) {
+        this.homework.put(index, true);
+    }
+
+}
