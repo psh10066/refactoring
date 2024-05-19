42d7e312ce0ff473ff451e17d4a4926c0f732bf1
이호준 <psh10066@gmail.com>
2024-05-20 00:08:33 +0900

    냄새 3. 긴 함수

---
+++ src/main/java/com/psh10066/refactoring/_03_long_function/_01_before/Participant.java
@@ -0,0 +1,14 @@
+package com.psh10066.refactoring._03_long_function._01_before;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public record Participant(String username, Map<Integer, Boolean> homework) {
+    public Participant(String username) {
+        this(username, new HashMap<>());
+    }
+
+    public void setHomeworkDone(int index) {
+        this.homework.put(index, true);
+    }
+}
