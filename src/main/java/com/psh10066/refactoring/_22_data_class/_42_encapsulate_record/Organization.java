ddb629f6eae7e5300e48e25a700bc2a83f63f154
이호준 <hj.lee@60hz.io>
2024-05-28 20:00:21 +0900

    리팩토링 42. 레코드 캡슐화하기

---
+++ src/main/java/com/psh10066/refactoring/_22_data_class/_42_encapsulate_record/Organization.java
@@ -0,0 +1,7 @@
+package com.psh10066.refactoring._22_data_class._42_encapsulate_record;
+
+public /*class*/ record Organization(String name, String country) {
+
+//    public String name;
+//    public String country;
+}
